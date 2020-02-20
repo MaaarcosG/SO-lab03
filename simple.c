c9873489f0a15004c85a86a53474d5fb10390320
Marcos Gutierrez <gut17909@uvg.edu.gt>
2020-02-19 19:40:57 -0700

    update exercise 3

--- simple.c
+++ simple.c
@@ -4,12 +4,18 @@
 #include <linux/list.h>
 
 /*Primer Metodo*/
-int simple_init(){
+int simple_init(void){
 	printk(KERN_INFO "Loading Module \nHola MUndo!");
-	return 0:
+	return 0;
 }
 
-void simple_exit(){
-	printk(KERN_INFO "Removing Module \nHola MUndo!");
-	return NULL;
+/*Segundo metodo*/
+void simple_exit(void){
+	printk(KERN_INFO "Removing Module \nHola Mundo!");
 }
+
+module_init(simple_init);
+module_exit(simple_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Lab 03 SO");
+MODULE_AUTHOR("Marcos Gutierrez");
